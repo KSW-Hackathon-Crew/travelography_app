43166e1a5ac221fdca6d776a1ba0039c5c1ea915
freejacque <freejacque@gmail.com>
2014-12-06 20:23:27 -0500

    added text field to activity table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141206205903) do
+ActiveRecord::Schema.define(version: 20141207012106) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20141206205903) do
     t.integer  "plan_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "text"
   end
 
   add_index "activities", ["plan_id"], name: "index_activities_on_plan_id", using: :btree
