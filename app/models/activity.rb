fc174c740a5706e5f7fe2ee620d94fd0cf6bfa52
freejacque <freejacque@gmail.com>
2014-12-07 01:13:41 -0500

    added relational code to models

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,3 +1,4 @@
 class Activity < ActiveRecord::Base
   belongs_to :plan
+  has_many :photos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  has_many :plans
   validates :email, presence: true, uniqueness: true
   has_secure_password
 
